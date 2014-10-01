cba1305953bce0d632f89b925da1426777a35c2c
HFausta <henry.fausta@gmail.com>
2014-10-01 12:17:48 +0700

    Created dummy login page

--- CVFormatter/settings.py
+++ CVFormatter/settings.py
@@ -26,7 +26,6 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = (
@@ -81,3 +80,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),    
+)
+
+STATICFILES_FINDERS = ( 
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+# import account model
+from registration.models import Account
 
 # Create your views here.
+def index(request):
+	return render(request, 'registration/index.html')
+	
+def register(request):
+	return HttpResponse("Registration page")
+	
